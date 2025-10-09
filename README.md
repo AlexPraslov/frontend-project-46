134b4f1cae8cbeac38e01969371f59e8615317b6
Alex <Jazz163rus@gmail.com>
2025-10-09 20:37:23 +0300

    Step 4 Finish

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/AlexPraslov/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/AlexPraslov/frontend-project-46/actions)
+
+### Демонстрация работы gendiff
+
+[![asciicast](https://asciinema.org/a/duytKSkTSvp4Th5RCWRQBjHRw)](https://asciinema.org/a/duytKSkTSvp4Th5RCWRQBjHRw)
--- gendiff.js
+++ gendiff.js
@@ -2,14 +2,39 @@
 
 import { Command } from 'commander';
 import { getDataFromFile } from './src/parsers.js';
+import _ from 'lodash';
 
+// Функция сравнения (добавляем только её)
+const buildDiff = (data1, data2) => {
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+  
+  const diffLines = keys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+    
+    if (!_.has(data2, key)) {
+      return `  - ${key}: ${value1}`;
+    }
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${value2}`;
+    }
+    if (value1 !== value2) {
+      return [`  - ${key}: ${value1}`, `  + ${key}: ${value2}`];
+    }
+    return `    ${key}: ${value1}`;
+  });
+  
+  return `{\n${diffLines.flat().join('\n')}\n}`;
+};
+
+// Основная функция (обновляем только её)
 const genDiff = (filepath1, filepath2) => {
   const data1 = getDataFromFile(filepath1);
   const data2 = getDataFromFile(filepath2);
-  
-  return `Comparing ${filepath1} and ${filepath2}\nData1: ${JSON.stringify(data1)}\nData2: ${JSON.stringify(data2)}`;
+  return buildDiff(data1, data2); // теперь возвращаем diff
 };
 
+// CLI часть без изменений
 const program = new Command();
 
 program
